5246627fbb88459219b7eea3d77e10cbd1a82e9b
Vasilii Kovalenko <vaselite10@yandex.ru>
2024-03-07 19:38:23 +0300

    Delete req.sh

--- req.sh
+++
@@ -1,7 +0,0 @@
-# For latest mysql-apt repository, check url from https://dev.mysql.com/downloads/repo/apt/
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb -O /tmp/mysql-apt-config_all.deb
-DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/mysql-apt-config_all.deb
-apt-get update
-apt-get install git clang cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev mysql-server p7zip
-update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
-update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang 100
