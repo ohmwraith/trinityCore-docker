7569a440d7a2ea55308220f7eb02d94ee01fe633
Vasilii Kovalenko <vaselite10@yandex.ru>
2024-03-07 19:32:46 +0300

    Update req.sh

--- req.sh
+++ req.sh
@@ -1,5 +1,5 @@
 # For latest mysql-apt repository, check url from https://dev.mysql.com/downloads/repo/apt/
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb -O /tmp/mysql-apt-config_all.deb
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb -O /tmp/mysql-apt-config_all.deb
 DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/mysql-apt-config_all.deb
 apt-get update
 apt-get install git clang cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev mysql-server p7zip
